36d2a9e0f5d02610dfa0d3e8af25aff46138461b
hun <wanted0301@gmail.com>
2020-10-05 17:46:25 +0900

    add forgot password page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
-import { Box, Button } from '@chakra-ui/core';
+import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useLoginMutation } from '../generated/graphql';
@@ -8,15 +8,16 @@ import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
+import NextLink from 'next/link';
 
 const Login: React.FC<{}> = ({}) => {
   const router = useRouter();
   const [, login] = useLoginMutation();
   return (
-    <Wrapper variant= "small">
+    <Wrapper variant="small">
       <Formik
         initialValues={{ usernameOrEmail: "", password: "" }}
-        onSubmit={async (values, { setErrors }) =>{
+        onSubmit={async (values, { setErrors }) => {
           const response = await login(values);
           if (response.data?.login.errors) {
             setErrors(toErrorMap(response.data.login.errors));
@@ -41,8 +42,13 @@ const Login: React.FC<{}> = ({}) => {
                 type="password"
               />
             </Box>
+            <Flex ml={2}>
+              <NextLink href="/forgot-password">
+                <Link ml="auto">forgot password?</Link>
+              </NextLink>
+            </Flex>
             <Button
-              mt={4} 
+              mt={4}
               type="submit"
               isLoading={isSubmitting}
               variantColor="teal"
